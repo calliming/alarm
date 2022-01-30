51474e0ed94bbf1bc7d767867df8f0a4fb01d28b
calliming <98711515+calliming@users.noreply.github.com>
2022-01-30 23:27:57 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,16 @@
-for (let index = 0; index < 4; index++) {
+basic.showLeds(`
+    . . . . .
+    . # . . .
+    . . # . .
+    . . . . .
+    . . . # .
+    `)
+while (true) {
+    basic.setLedColor(0x0000ff)
     music.playTone(247, music.beat(BeatFraction.Whole))
+    basic.setLedColor(0xffffff)
     music.playTone(330, music.beat(BeatFraction.Whole))
+    if (input.buttonIsPressed(Button.B)) {
+        break;
+    }
 }
