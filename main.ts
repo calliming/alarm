e23f3e44be8390437c0b7970bf4b00438739a77e
calliming <98711515+calliming@users.noreply.github.com>
2022-01-31 20:02:30 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,14 @@
-basic.showLeds(`
-    . . . . .
-    . # . . .
-    . . # . .
-    . . . . .
-    . . . # .
-    `)
-while (true) {
-    basic.setLedColor(0x0000ff)
-    music.playTone(247, music.beat(BeatFraction.Whole))
-    basic.setLedColor(0xffffff)
-    music.playTone(330, music.beat(BeatFraction.Whole))
-    if (input.buttonIsPressed(Button.B)) {
+let i = 0
+music.playMelody("C E D F E G E F ", 120)
+while (i <= 10) {
+    basic.showNumber(i)
+    music.playMelody("C E D F E G E F ", 360)
+    i += 1
+    // basic.setLedColor(0x0000ff)
+    // music.playTone(247, music.beat(BeatFraction.Whole))
+    // basic.setLedColor(0xffffff)
+    // music.playTone(330, music.beat(BeatFraction.Whole))
+    if (input.buttonIsPressed(Button.A)) {
         break;
     }
 }
