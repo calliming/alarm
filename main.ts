76a1a0a967393c271387f04a5cfe0a3879fcdba8
calliming <98711515+calliming@users.noreply.github.com>
2022-02-04 15:39:01 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
-let i = 0
-music.playMelody("C E D F E G E F ", 120)
-while (i <= 10) {
-    basic.showNumber(i)
-    music.playMelody("C E D F E G E F ", 360)
-    i += 1
-    // basic.setLedColor(0x0000ff)
-    // music.playTone(247, music.beat(BeatFraction.Whole))
-    // basic.setLedColor(0xffffff)
-    // music.playTone(330, music.beat(BeatFraction.Whole))
-    if (input.buttonIsPressed(Button.A)) {
-        break;
-    }
+for (let index = 0; index < 4; index++) {
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    music.playTone(330, music.beat(BeatFraction.Whole))
 }
+for (let index = 0; index < 4; index++) {
+    basic.setLedColor(0x0000ff)
+    music.playTone(247, music.beat(BeatFraction.Whole))
+    basic.turnRgbLedOff()
+    music.playTone(330, music.beat(BeatFraction.Whole))
+}
+for (let index = 0; index < 4; index++) {
+    basic.setLedColor(0x0000ff)
+    basic.pause(500)
+    basic.turnRgbLedOff()
+    basic.pause(500)
+}
+basic.turnRgbLedOff()
